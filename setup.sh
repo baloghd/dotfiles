e75dee600a992df8f652c17b126ebc792bd6a998
Balogh Domonkos <balogh.domonkos@gmail.com>
2022-09-13 18:43:40 +0200

    zsh, ibm plex

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt update
+
+# install and setup zsh
+sudo apt install zsh zsh-common zsh-autosuggestions zsh-syntax-highlighting -y
+
+# install IBM Plex Mono
+sudo apt install fonts-ibm-plex -y 
+
+# install vim, git
+
+# install autojump
