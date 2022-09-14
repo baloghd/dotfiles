963b4c1a25932ea3233cc78a8ff63bc608265d12
Balogh Domonkos <balogh.domonkos@gmail.com>
2022-09-14 10:38:15 +0200

    vim git curl

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt install zsh zsh-common zsh-autosuggestions zsh-syntax-highlighting -y
 # install IBM Plex Mono
 sudo apt install fonts-ibm-plex -y 
 
-# install vim, git
+# install vim, git, curl
+sudo apt install vim git curl -y 
 
 # install autojump
