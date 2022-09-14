1589495ffe4bf4b6e8ee2cbb147d82f4f5545641
Balogh Domonkos <balogh.domonkos@gmail.com>
2022-09-14 12:22:56 +0200

    order fix

--- setup.sh
+++ setup.sh
@@ -3,13 +3,20 @@ sudo apt update
 # install leftover configuration, if there is any
 sudo dpkg --configure -a
 
+# install git, curl
+sudo apt install git curl -y 
+
+# install and setup vim 
+sudo apt install vim -y
+
 # install and setup zsh
 sudo apt install zsh zsh-common zsh-autosuggestions zsh-syntax-highlighting -y
+bash -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
 
-# install IBM Plex Mono
+# install IBM Plex Mono and set it as default monospace font
 sudo apt install fonts-ibm-plex -y 
 
-# install vim, git, curl
-sudo apt install vim git curl -y 
-
 # install autojump
+sudo apt install autojump
+# add activation to .zshrc
+echo ". /usr/share/autojump/autojump.sh" >> .zshrc
