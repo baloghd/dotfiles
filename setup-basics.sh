b0ad172c895f3ead110b8786d4efe600bec47ad1
baloghd <baloghd@sent.com>
2024-03-13 21:02:19 +0100

    spotify snap -> deb

--- setup-basics.sh
+++ setup-basics.sh
@@ -56,7 +56,9 @@ code --install-extension coolbear.systemd-unit-file
 
 
 # spotify
-sudo snap install spotify 
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 # noise generators
 sudo apt install sox
