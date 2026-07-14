a813d9cdf1b4faeaec6dac9f01f97ff1b1bf6377
baloghd <baloghd@sent.com>
2026-07-14 22:03:03 +0200

    refactor: move homelab-coupled configs to homelab repo
    - setup-mounts.sh, setup-hosts.sh, setup-own-systemd-services.sh (and systemd/)
    - syncthing/ stack
    - yt-vpn/ stack
    
    Also remove stale syncthing/config/config.xml (placeholder, regenerated by
    syncthing on first run).
    
    See homelab repo commit 36bea1a for the corresponding import.

--- fedora/setup-own-systemd-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-USER=xcvb
-
-sed "s/__USERNAME__/$USER/g" systemd/pass-dropbox.service > /etc/systemd/system/pass-dropbox.service
-
-sed "s/__USERNAME__/$USER/g" systemd/obsidian-sync.service > /etc/systemd/system/obsidian-sync.service
--- ubuntu/syncthing/ports.sh
+++
@@ -1,3 +0,0 @@
-sudo ufw allow 8384 && sudo ufw allow 22000/tcp && sudo ufw allow 22000/udp && sudo ufw allow 21027/udp
-sudo ufw reload
-sudo ufw status
